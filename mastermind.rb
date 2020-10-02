7a2aa6681201753cb821660345b024c65ff132ad
mittalrohit0598 <mittalrohit0598@gmail.com>
2020-10-02 12:28:55 +0530

    Add functionality to set the secret code to be guessed

--- mastermind.rb
+++ mastermind.rb
@@ -14,6 +14,11 @@ class Computer < Player
   end
 
   def set_code
+    code = []
+    4.times do
+      code << rand(1..4)
+    end
+    code.join('')
   end
 end
 
@@ -29,10 +34,11 @@ end
 
 # class Game
 class Game
-  attr_accessor :player1, :player2, :code_setter, :code
-  def initialize(player1 = Player.new('Human'), player2 = Computer.new('Computer'))
-    @human = player1
-    @computer = player2
+  attr_accessor :human, :computer, :code_setter, :code
+  def initialize(human = Player.new('Human'), computer = Computer.new('Computer'))
+    @human = human
+    @computer = computer
+    @code_setter = ''
     @code = set_code
   end
 
@@ -43,9 +49,28 @@ class Game
   end
 
   def set_code
+    puts 'Do you want to be the creator or the guesser of the secret code(c or g)?'
+    loop do
+      self.code_setter = gets.chomp.downcase
+      break if %w[c g].include?(code_setter)
+
+      puts 'Invalid input! Try again.'
+    end
+    return computer.set_code if code_setter == 'g'
+
+    puts 'Enter your secret code of size 4(digits 1-5 allowed): '
+    code = ''
+    loop do
+      code = gets.chomp
+      break if valid_code?(code)
+
+      puts 'Invalid input! Try again.'
+    end
+    code
   end
 
-  def valid_code?
+  def valid_code?(code)
+    code.split('').all? { |digit| digit.to_i <= 5 && digit.to_i >= 1 } && code.length == 4
   end
 
   def game_over_message
